69c9f13cccb23672bd0a3c4c52c5891dcc685c0e
Fakhar <127673611+fakhurdin@users.noreply.github.com>
2025-04-28 12:31:57 +0500

    Create install_packages.sh

---
+++ scripts/install_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# SysMate - Install Essential Packages Script
+set -e
+
+PACKAGES="curl wget vim git ufw net-tools htop unzip"
+
+echo "----------------------------------"
+echo "Updating package repositories..."
+sudo apt update
+
+echo "Installing essential packages: $PACKAGES"
+sudo apt install -y $PACKAGES
+
+echo "Package installation complete!"
+echo "----------------------------------"
